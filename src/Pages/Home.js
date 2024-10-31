36ba0ecec75591296f461c2d64eaa5f6e3202237
bardobardo21 <youssefmarzouk2112@gmail.com>
2024-10-31 15:47:18 -0400

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
         <Box>
           <Link to='/auth/login'>
             <Typography sx={{ fontWeight: '500', color: '#1a73e8' }}>
-              Login to Dashboard
+              Login to Fortnite
             </Typography>
           </Link>
         </Box>
